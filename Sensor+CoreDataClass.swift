61ec665fb7c146f5b39c98540342dbb074efa05c
moonsoungjin <seongjinmun74@gmail.com>
2022-12-27 17:30:31 +0900

    feat: 코어 데이터 생성

---
+++ Sensor+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Sensor+CoreDataClass.swift
+//  GyroData
+//
+//  Created by 문성진 on 2022/12/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Sensor)
+public class Sensor: NSManagedObject {
+
+}
