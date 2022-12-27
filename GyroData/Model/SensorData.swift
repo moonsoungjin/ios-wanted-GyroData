e0ebf8da7d08cbe119cd1a56fabf6f281271b401
moonsoungjin <seongjinmun74@gmail.com>
2022-12-27 17:39:26 +0900

    feat: SensorData 모델 생성

---
+++ GyroData/Model/SensorData.swift
@@ -0,0 +1,14 @@
+//
+//  Sensor.swift
+//  GyroData
+//
+//  Created by 문성진 on 2022/12/27.
+//
+
+import Foundation
+
+struct SensorData {
+    var measurementDate: String
+    var sensorValue: Double
+    var sensorName: String
+}
