7d005c992a1a67bd5ec4322f3c9f40e7839e4162
moonsoungjin <seongjinmun74@gmail.com>
2022-12-27 23:08:28 +0900

    feat: UI로 코드를 짜기위한 설정 및 FirstViewController 생성

---
+++ GyroData/Controller/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  GyroData
+//
+//  Created by 문성진 on 2022/12/27.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
