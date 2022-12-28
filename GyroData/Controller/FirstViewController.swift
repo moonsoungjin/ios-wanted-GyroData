2056927b7c768accfa5a203d5b9289990400a782
moonsoungjin <seongjinmun74@gmail.com>
2022-12-28 11:45:04 +0900

    feat: tableview 생성

--- GyroData/Controller/FirstViewController.swift
+++ GyroData/Controller/FirstViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    let sensorTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        self.setSensorTableViewLayout()
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setSensorTableViewLayout() {
+        self.view.addSubview(sensorTableView)
+        sensorTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+                    sensorTableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                    sensorTableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                    sensorTableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                    sensorTableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+                ])
     }
-    */
+
+    
 
 }
